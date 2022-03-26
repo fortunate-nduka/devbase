1df29c3bc9993daef6cc1208937738cdac6a6b7b
Fortunate ogodu <ogodujoseph@gmail.com>
2022-03-26 16:22:55 +0100

    updated commit

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -30,24 +30,25 @@ const Posts = () => {
   }, []);
 
   return (
-    <div className='px-5 py-10 bg-[#f3f4f5] grid gap-x-5 gap-y-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+    <div className='px-2 sm:px-10 md:px-3 py-10 bg-[#f3f4f5] grid gap-x-5 gap-y-14 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
       {posts.map((post) => (
         <div key={post._id} className='shadow-2xl rounded-md'>
           {post.mainImage && post.mainImage.asset && (
             <img
               src={post.mainImage.asset.url}
               alt=''
-              className='rounded-tl-md rounded-tr-md'
+              className='object-fit h-80 w-full'
             />
           )}
-          <div className='bg-white px-3 py-5 rounded-bl-md rounded-br-md'>
+          <div className='bg-white px-3 sm:px-5 py-6'>
             <Moment
-              className='text-sm font-semibold text-gray-500 tracking-wider'
-              dateToFormat={post.publishedAt}
               fromNow
-            />
-            <h2 className='font-black text-2xl pt-2 pb-2'>{post.title}</h2>
-            <div className='flex text-sm font-semibold text-gray-500 tracking-wider pb-2'>
+              className='text-sm font-medium text-gray-500 tracking-wider'
+            >
+              {post.publishedAt} 
+            </Moment>
+            <h2 className='font-black tracking-wide text-2xl pt-3 pb-2'>{post.title}</h2>
+            <div className='flex gap-2 items-center text-sm font-medium text-gray-500 tracking-wider pb-3'>
               By
               {post.author.image && post.author.image.asset && (
                 <img
@@ -58,9 +59,12 @@ const Posts = () => {
               )}
               <div className=''>{post.author.name}</div>
             </div>
-            <p className='text-sm font-semibold text-gray-500 tracking-wider'>
-              {post.description.slice(0, 200)}
+            <p className='text-sm font-medium text-gray-500 tracking-wider pb-6'>
+              {post.description.slice(0, 200)}...
             </p>
+            <button className='bg-black py-3 px-6 text-sm font-semibold text-white cursor-pointer tracking-widest'>
+              Read more
+            </button>
           </div>
         </div>
       ))}
