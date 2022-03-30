855874b71fa3be2a2c60c5c039d308e2347742d2
Fortunate ogodu <ogodujoseph@gmail.com>
2022-03-30 05:26:14 +0100

    updated commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,17 @@
 const Footer = () => {
   return (
-    <div className='mt-[5rem] shadow-2xl flex items-center justify-center w-full bg-white h-14 text-xs font-medium'>
-      &copy; 2022 | All rights reserved | Developed by
-      <span className='text-blue-500 underline pl-1'>iamfortunate</span>
+    <div className='mt-[5rem] shadow-2xl flex items-center justify-center w-full bg-white h-14 text-xs font-medium flex-col sm:flex-row gap-y-1'>
+      &copy; 2022 | All rights reserved{' '}
+      <div className='hidden sm:inline-block sm:pl-1'>|</div>
+      <div className='sm:pl-1'>
+        Developed with{' '}
+        <i
+          className='em em-hearts'
+          aria-label='BLACK HEART SUIT'
+        ></i>{' '}
+        by
+        <span className='text-blue-500 underline pl-1'>iamfortunate</span>
+      </div>
     </div>
   );
 };
